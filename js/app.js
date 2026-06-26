cb483ac2cea6db45af7c15be2eb9cdf8b44e8a8d
Mehedimid <mehedihasan25276@gmail.com>
2026-06-27 02:38:30 +0600

    added remove an item or product from cart functionality

--- js/app.js
+++ js/app.js
@@ -28,17 +28,23 @@ function displayMenu(items) {
     })
     .join("");
 }
+// showing menu items ends == x ==
 
-// showing the cart =====>
+// calling menu items
+displayMenu(menuItems);
+
+// showing the cart starts =====>
 function displayCart() {
   if (cart.length === 0) {
-    cartContainer.innerHTML = "<div><h2>Shopping Cart</h2><p>Your cart is empty.</p></div>";
+    cartContainer.innerHTML =
+      "<div><h2>Shopping Cart</h2><p>Your cart is empty.</p></div>";
     return;
-  } 
+  }
 
-const cartHTML = cart.map((item) => {
-    return `
-<div class="cart-item">
+  const cartHTML = cart
+    .map((item) => {
+      return `
+      <div class="cart-item">
         <img src="${item.image}" alt="${item.name}" class="cart-item-img">
         
         <div class="cart-item-details">
@@ -46,15 +52,18 @@ const cartHTML = cart.map((item) => {
           <p>Qty: ${item.quantity}</p>
           <p>$${(item.price * item.quantity).toFixed(2)}</p>
         </div>
+
+        <button class="remove-item-btn" data-id="${item.id}">❌</button>
       </div>
     `;
-  }).join(" ");
+    })
+    .join(" ");
 
   const totalAmount = cart.reduce((accumulator, item) => {
-
-    return accumulator + (item.price * item.quantity);
+    return accumulator + item.price * item.quantity;
   }, 0);
 
+  // cart container under display cart function - starts===
   cartContainer.innerHTML = `
     <h2>Shopping Cart</h2>
     ${cartHTML}
@@ -63,27 +72,34 @@ const cartHTML = cart.map((item) => {
       <h3 id="cart-total-amount">$${totalAmount.toFixed(2)}</h3>
     </div>
   `;
+  // cart container under display cart function - ends
+
 }
+// showing cart ends  == x ==
 
-displayMenu(menuItems);
-
-function displaySearchItems (searchItem) {
+// showing search Items starts =====>
+function displaySearchItems(searchItem) {
   console.log(searchItem);
-  const selectedSearchItems = menuItems.filter(item => item.name.toLowerCase().includes(searchItem))
-  console.log(selectedSearchItems);
-  displayMenu(selectedSearchItems)
-}
+  const selectedSearchItems = menuItems.filter((item) =>
+    item.name.toLowerCase().includes(searchItem),
+  );
 
-searchInput.addEventListener("input", (event)=>{
-  const searchItem = event.target.value.toLowerCase()
-  displaySearchItems(searchItem)
-})
+  displayMenu(selectedSearchItems);
+}
+// showing search Items ends ==== X ====
+
+// input container starts ===
+searchInput.addEventListener("input", (event) => {
+  const searchItem = event.target.value.toLowerCase();
+  displaySearchItems(searchItem);
+});
+
 
 // create cart array=====>
 const cart = [];
 
+// menu container starts ====>
 menuContainer.addEventListener("click", (event) => {
-
   const id = Number(event.target.dataset.id);
 
   if (!id) return;
@@ -98,10 +114,23 @@ menuContainer.addEventListener("click", (event) => {
     const newItem = { ...selectedItem, quantity: 1 };
     cart.push(newItem);
   }
-  console.log(cart);
+  // console.log(cart);
   displayCart();
 });
+// menu container ends === X ===
 
+  // get deleted item id ====
+ cartContainer.addEventListener("click", (event) => {
+  if(event.target.classList.contains("remove-item-btn")){
+   const deletedId = Number(event.target.dataset.id)
 
+    const deletedItemIndex = cart.findIndex(item=>item.id === deletedId)
 
+    if(deletedItemIndex!== -1){
+      cart.splice(deletedItemIndex, 1)
+      displayCart()
+    }
+  }
+
+  });
 
--- js/practice.js
+++ js/practice.js
@@ -1,2 +1,7 @@
- const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
- console.log(alphabet);
+ const alphabet = "ABCDE".split("")
+   const newAlphabets2 = alphabet.map(element =>{
+    return "helllooo"
+   });
+ console.log(newAlphabets2);
+ const newAlphabets = alphabet.forEach(element =>console.log(element+" hello"));
+ console.log(newAlphabets);
